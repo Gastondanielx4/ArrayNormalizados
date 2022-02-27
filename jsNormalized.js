5ed29f4e63c7a9b7d006ced87ceac7666a0cf924
Gastondanielx4 <tochircentral@gmail.com>
2022-02-27 16:40:40 -0300

    actualizacion commit 4

--- jsNormalized.js
+++ jsNormalized.js
@@ -30,9 +30,9 @@ MyStudents.forEach(element => {
         crearHTML += `
             <div id="card${element.id}" class="col">
                 <div class="card text-center" style=" height:15rem; width: 18rem; padding-top: 1rem; align-items: center;">
-                    <img src="${element.avatar||element.icon}" class="card-img-top" alt="..." style="border-radius: 100%; width: 5rem">
+                    <img src="${element.avatar}" class="card-img-top" alt="..." style="border-radius: 100%; width: 5rem">
                     <div class="card-body">
-                    <h5 class="card-title">${element.firstName||element.name}</h5>
+                    <h5 class="card-title">${element.name}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">${element.title}</h6>
                     <a href="#" class="btn btn-primary">${element.buttontext}</a>
                     </div>
